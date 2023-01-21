43386265491cda98afe09c1c6dd413c8713db32c
MarcusMogg <m1664907189@gmail.com>
2023-01-21 18:42:46 +0800

    feat: add werror

--- src/xmake.lua
+++ src/xmake.lua
@@ -4,6 +4,7 @@ target("cwheel_lib")
     set_kind("static")
     set_languages("cxx20")
     set_toolchains("clang")
+    set_warnings("all", "error")
     add_files("*.cpp","*.cc")
     add_includedirs("$(projectdir)/", {public = true})
 
