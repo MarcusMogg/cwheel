3f117dfbb8412f5c20ef055b48990816be1af5ed
Mogg <34648459+MarcusMogg@users.noreply.github.com>
2023-03-12 22:25:44 +0800

    test coverage (#2)

--- test/xmake.lua
+++ test/xmake.lua
@@ -5,10 +5,7 @@ add_requires("gtest")
 target("cwheel_test")
     set_kind("binary")
     set_languages("cxx20")
-    set_toolchains("clang")
     add_files("*.cc")
     add_packages("gtest")
     add_deps("cwheel_lib")
-    add_cxxflags("-fprofile-instr-generate", "-fcoverage-mapping") -- https://clang.llvm.org/docs/SourceBasedCodeCoverage.html
-    add_ldflags("-fprofile-instr-generate")
-
+    add_options("test")
--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,17 @@
 add_rules("mode.debug", "mode.release")
+add_rules("mode.coverage")
+
+option("test")
+    set_default(false)
+    set_showmenu(true)
+    add_cxxflags("-fprofile-arcs -ftest-coverage")
+option_end()    
+
+if has_config("test") then
+    set_toolchains("gcc")
+else
+    set_toolchains("clang")
+end
 
 includes("src")
 includes("test")
@@ -6,7 +19,7 @@ includes("test")
 target("cwheel")
     set_kind("binary")
     set_languages("cxx20")
-    set_toolchains("clang")
     add_deps("cwheel_lib")
     add_files("*.cpp","*.cc")
+    add_options("test")
 
