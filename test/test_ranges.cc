a83293dafd0c06922d53eccbdbdfdc6b722696df
MarcusMogg <m1664907189@gmail.com>
2023-03-12 22:13:30 +0800

    fix: rm ranges

--- test/test_ranges.cc
+++ test/test_ranges.cc
@@ -1,28 +1,28 @@
-#include <array>
-#include <iostream>
-#include <vector>
+// #include <array>
+// #include <iostream>
+// #include <vector>
 
-#include "gtest/gtest.h"
-#include "src/ranges.hpp"
+// #include "gtest/gtest.h"
+// #include "src/ranges.hpp"
 
-int F1(int a) { return a + 1; }
-int F2(int a) { return a * 2; }
+// int F1(int a) { return a + 1; }
+// int F2(int a) { return a * 2; }
 
-TEST(Ranges, ComposeTestBasicFunc) {
-  auto f1 = cwheel::Compose(F1, F2);
-  EXPECT_EQ(f1(1), 3);
+// TEST(Ranges, ComposeTestBasicFunc) {
+//   auto f1 = cwheel::Compose(F1, F2);
+//   EXPECT_EQ(f1(1), 3);
 
-  auto f2 = cwheel::Compose(F2, F1);
-  EXPECT_EQ(f2(1), 4);
-}
+//   auto f2 = cwheel::Compose(F2, F1);
+//   EXPECT_EQ(f2(1), 4);
+// }
 
-TEST(Ranges, ComposeTestLambda) {
-  auto f1 = cwheel::Compose(F1, [](int a) { return a * 2; });
-  EXPECT_EQ(f1(1), 3);
+// TEST(Ranges, ComposeTestLambda) {
+//   auto f1 = cwheel::Compose(F1, [](int a) { return a * 2; });
+//   EXPECT_EQ(f1(1), 3);
 
-  auto f2 = cwheel::Compose(F2, [](int a) { return a + 1; });
-  EXPECT_EQ(f2(1), 4);
-}
+//   auto f2 = cwheel::Compose(F2, [](int a) { return a + 1; });
+//   EXPECT_EQ(f2(1), 4);
+// }
 
 // TEST(Ranges, AllTest) {
 //   auto a = std::vector{1, 2, 3, 4};
