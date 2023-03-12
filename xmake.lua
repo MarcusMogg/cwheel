149d3fbb1b342b191b907c886672296f5e190ab7
MarcusMogg <m1664907189@gmail.com>
2023-03-12 21:44:33 +0800

    fix: use gcc

--- xmake.lua
+++ xmake.lua
@@ -1,15 +1,18 @@
 add_rules("mode.debug", "mode.release")
 add_rules("mode.coverage")
 
-set_toolchains("clang")
-
 option("test")
     set_default(false)
     set_showmenu(true)
     add_cxxflags("-fprofile-arcs -ftest-coverage")
     add_defines("DEBUG")
-    set_toolchains("gcc") -- for coverage
 option_end()    
+
+if is_config("test", true ) then
+    set_toolchains("gcc")
+else
+    set_toolchains("clang")
+end
 
 includes("src")
 includes("test")
